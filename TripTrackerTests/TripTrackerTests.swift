160c96357bd521b92163c4b80e51099981fb724c
Javier Pineda <jpgpineda98@gmail.com>
2024-05-13 06:36:57 -0600

    adding main test scenarios

--- TripTrackerTests/TripTrackerTests.swift
+++ TripTrackerTests/TripTrackerTests.swift
@@ -2,13 +2,76 @@
 //  TripTrackerTests.swift
 //  TripTrackerTests
 //
-//  Created by javier pineda on 11/05/24.
+//  Created by javier pineda on 13/05/24.
 //
 
 import XCTest
 @testable import TripTracker
 
-final class TripTrackerTests: XCTestCase {
-    func test
-
+final class TripTrackerTests: TripTrackerBaseTest {
+    
+    func testPostResponseNotEmpty() async {
+        // Arrange
+        let apiGateway = PostApiGatewayImplementation(apiClient: apiClient,
+                                                      storageContext: storageContext)
+        
+        let useCase = PostUseCaseImplementation(apiGateway: apiGateway)
+        
+        // ACt
+        let result = await useCase.fetchPosts(parameters: FetchPostsRequest())
+        
+        // Assert
+        switch result {
+        case .success(let posts):
+            XCTAssertFalse(posts.isEmpty)
+        case .failure(let apiError):
+            XCTFail("The test has failed: \(apiError.customDescription)")
+        }
+    }
+    
+    func testPostActionsNotEmpty() async {
+        // Arrange
+        let apiGateway = PostApiGatewayImplementation(apiClient: apiClient,
+                                                      storageContext: storageContext)
+        
+        let useCase = PostUseCaseImplementation(apiGateway: apiGateway)
+        
+        // Act
+        let result = await useCase.fetchPosts(parameters: FetchPostsRequest())
+        
+        // Assert
+        switch result {
+        case .success(let posts):
+            guard let post = posts.first else {
+                XCTFail("The test has failed due to empty data")
+                return
+            }
+            XCTAssertFalse(post.actions.isEmpty)
+        case .failure(let apiError):
+            XCTFail("The test has failed: \(apiError.customDescription)")
+        }
+    }
+    
+    func testCommentsNotEmpty() async {
+        // Arrange
+        let apiGateway = PostApiGatewayImplementation(apiClient: apiClient,
+                                                      storageContext: storageContext)
+        
+        let useCase = PostUseCaseImplementation(apiGateway: apiGateway)
+        
+        // Act
+        let result = await useCase.fetchPosts(parameters: FetchPostsRequest())
+        
+        // Assert
+        switch result {
+        case .success(let posts):
+            guard let post = posts.first else {
+                XCTFail("The test has failed due to empty data")
+                return
+            }
+            XCTAssertFalse(post.commets.isEmpty)
+        case .failure(let apiError):
+            XCTFail("The test has failed: \(apiError.customDescription)")
+        }
+    }
 }
